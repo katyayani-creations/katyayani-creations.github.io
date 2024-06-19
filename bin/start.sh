a6f5cdc462ad1196ac3d54c997cd16d846acf194
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-20 01:51:50 +0530

    update

--- bin/start.sh
+++ bin/start.sh
@@ -11,26 +11,26 @@ function changeURL() {
 }
 
 function clean() {
-    # minify -o index.html index.html
     sed -i -e "s/^[ \t]*//g" $1.html
     sed -i -e "/^$/d" $1.html
     sed -i -e "s/^[ \t]*//g" $1.html
-    sed -i -e "s/<!---* *<\(.*\)> *-->/<\1>/" $1.html
 }
 
 # wget https://katyayani-creations.in/prod.php -o /dev/null  && mv prod.php prod.txt
 # sed -i -e 's/##/\n/g' prod.txt
 
-# wget https://katyayani-creations.in/cat.php -o /dev/null  && mv cat.php cat.txt
-# sed -i -e 's/##/\n/g' cat.txt
+wget https://katyayani-creations.in/cat.php -o /dev/null  && mv cat.php cat.txt
+sed -i -e 's/##/\n/g' cat.txt
 
 wget https://katyayani-creations.in/index.php -o /dev/null && mv index.php index.html
 clean "index"
 
-# while IFS= read -r str; do
-#     echo "downloading file: $str"
-#     wget https://katyayani-creations.in/$str -o /dev/null && mv $str $str.html 
-# done < cat.txt
+if [ -f cat.txt ]; then
+    while IFS= read -r str; do
+        echo "downloading file: $str"
+        wget https://katyayani-creations.in/$str -o /dev/null && mv $str $str.html 
+    done < cat.txt
+fi
 
 if [ -f prod.txt ]; then
     while IFS= read -r str; do
