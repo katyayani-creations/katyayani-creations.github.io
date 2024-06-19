2fbb717e5d5a8626d526307140a59cc300613d9f
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-19 19:52:25 +0530

    update

--- bin/start.sh
+++ bin/start.sh
@@ -12,18 +12,20 @@ categoryArr=(
 
 function changeURL() {
     for str in ${categoryArr[@]}; do
-        sed -i -e 's/https:\/\/katyayani-creations.in\/'$str'/https:\/\/katyayani-creations.github.io\/'$str'/g' $str.html
+        for str1 in ${categoryArr[@]}; do
+            sed -i -e 's/https:\/\/katyayani-creations.in\/'$str'/https:\/\/katyayani-creations.github.io\/'$str'/g' $str1.html
+            sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $str1.html
+        done
         sed -i -e 's/https:\/\/katyayani-creations.in\/'$str'/https:\/\/katyayani-creations.github.io\/'$str'/g' index.html
-        sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $str.html
         sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' index.html
     done    
 }
 
-wget https://katyayani-creations.in/index.php && mv index.php index.html
+# wget https://katyayani-creations.in/index.php && mv index.php index.html
 
-for str in ${categoryArr[@]}; do
-    wget https://katyayani-creations.in/$str && mv $str $str.html
-done 
+# for str in ${categoryArr[@]}; do
+#     wget https://katyayani-creations.in/$str && mv $str $str.html
+# done 
 
 changeURL
 
