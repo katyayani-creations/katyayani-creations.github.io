391aeee81b5bb1826c182a3cc30149419bf37194
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-19 18:56:13 +0530

    update

--- bin/start.sh
+++ bin/start.sh
@@ -1,12 +1,12 @@
 function download() {
-    wget https://katyayani-creations.in/$1.php && mv $1.php $1.html
-    sed -i -e 's/https:\/\/katyayani-creations.in\/contact/https:\/\/katyayani-creations.github.io\/contact/g' $1.html
-    sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $1.html
-
-    
+    wget https://katyayani-creations.in/$1.php && mv $1.php $2.html
+    sed -i -e 's/https:\/\/katyayani-creations.in\/contact/https:\/\/katyayani-creations.github.io\/contact/g' $2.html
+    sed -i -e 's/https:\/\/katyayani-creations.in\/terms-and-condition/https:\/\/katyayani-creations.github.io\/terms-and-condition/g' $2.html
+    sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $2.html
 }
 
-download "index"
-download "contact"
+download "index" "index"
+download "contact" "index"
+download "terms" "terms-and-condition"
 
 git commit -am "update" && git push origin main
