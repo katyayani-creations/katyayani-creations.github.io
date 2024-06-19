cee968b44ed8d23b55b7eb04acbfd312924a7611
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-19 21:09:48 +0530

    update

--- bin/start.sh
+++ bin/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 function changeURL() {
     while IFS= read -r str; do
-        sed -i -e 's/https:\/\/katyayani-creations.in\//https:\/\/katyayani-creations.github.io/g' $str.html
+        sed -i -e 's/https:\/\/katyayani-creations.in/https:\/\/katyayani-creations.github.io/g' $str.html
         sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $str.html
     done < cat.txt 
-    sed -i -e 's/https:\/\/katyayani-creations.in\//https:\/\/katyayani-creations.github.io/g' index.html
+    sed -i -e 's/https:\/\/katyayani-creations.in/https:\/\/katyayani-creations.github.io/g' index.html
     sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' index.html
 }
 
-# wget https://katyayani-creations.in/prod.php -o /dev/null  && mv prod.php prod.txt
-# sed -i -e 's/##/\n/g' prod.txt
+wget https://katyayani-creations.in/prod.php -o /dev/null  && mv prod.php prod.txt
+sed -i -e 's/##/\n/g' prod.txt
 
 # wget https://katyayani-creations.in/cat.php -o /dev/null  && mv cat.php cat.txt
 # sed -i -e 's/##/\n/g' cat.txt
@@ -26,12 +26,12 @@ while IFS= read -r str; do
     # echo ${arrIN[0]} 
     eval "mkdir -p ${arrIN[0]}"
     echo "downloading file: $str"
-    wget https://katyayani-creations.in/$str -o /dev/null -O $str  && mv $str $str.html 
+    wget https://katyayani-creations.in/$str -o /dev/null -O $str && mv $str $str.html 
     break
 done < prod.txt
 
-changeURL
+# changeURL
 
-rm -rf *-e cat.txt prod.txt
-git add .
-git commit -am "update" && git push origin main
+# rm -rf *-e cat.txt prod.txt
+# git add .
+# git commit -am "update" && git push origin main
