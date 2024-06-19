85e52ae093ce68347863264e26edd6259d420310
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-19 20:31:40 +0530

    update

--- bin/start.sh
+++ bin/start.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# wget https://katyayani-creations.in/cat.php  && mv cat.php cat.txt
+wget https://katyayani-creations.in/cat.php  && mv cat.php cat.txt
 sed -i -e 's/##/\n/g' cat.txt
 
 function changeURL() {
     while IFS= read -r str; do
-        while IFS= read -r str1; do
-            sed -i -e 's/https:\/\/katyayani-creations.in\/'$str'/https:\/\/katyayani-creations.github.io\/'$str'/g' $str1.html
-            sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $str1.html
-        done < cat.txt
-        sed -i -e 's/https:\/\/katyayani-creations.in\/'$str'/https:\/\/katyayani-creations.github.io\/'$str'/g' index.html
-        sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' index.html
+        sed -i -e 's/https:\/\/katyayani-creations.in\//https:\/\/katyayani-creations.github.io/g' $str.html
+        sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $str.html
     done < cat.txt 
+    sed -i -e 's/https:\/\/katyayani-creations.in\//https:\/\/katyayani-creations.github.io/g' index.html
+    sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' index.html
 }
 
 wget https://katyayani-creations.in/index.php && mv index.php index.html
 
 while IFS= read -r str; do
-    echo "Text read from file: $str"
+    echo "downloading file: $str"
     wget https://katyayani-creations.in/$str && mv $str $str.html
 done < cat.txt
 
