0ed9af75afa5562286630f21106466dbd7707726
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-19 19:30:56 +0530

    update

--- bin/start.sh
+++ bin/start.sh
@@ -1,28 +1,30 @@
+categoryArr=(
+    "contact" 
+    "terms-and-conditions" 
+    "toys"
+    "stationery"
+    "gift-packs"
+    "return-gifts"
+    "board-games"
+    "products"
+)
+
 function changeURL() {
-    sed -i -e 's/https:\/\/katyayani-creations.in\/contact/https:\/\/katyayani-creations.github.io\/contact/g' $1.html
-    sed -i -e 's/https:\/\/katyayani-creations.in\/terms-and-condition/https:\/\/katyayani-creations.github.io\/terms-and-condition/g' $1.html
-    sed -i -e 's/https:\/\/katyayani-creations.in\/toys/https:\/\/katyayani-creations.github.io\/toys/g' $1.html
-    sed -i -e 's/https:\/\/katyayani-creations.in\/stationery/https:\/\/katyayani-creations.github.io\/stationery/g' $1.html
-    sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $1.html
-}
-function download() {
-    wget https://katyayani-creations.in/$1.php && mv $1.php $2.html
-    changeURL $2
+    for str in ${categoryArr[@]}; do
+        sed -i -e 's/https:\/\/katyayani-creations.in\/'$str'/https:\/\/katyayani-creations.github.io\/'$str'/g' $str.html
+        sed -i -e 's/https:\/\/katyayani-creations.in\/'$str'/https:\/\/katyayani-creations.github.io\/'$str'/g' index.html
+        sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $str.html
+        sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' index.html
+    done    
 }
 
-function downloadCategory() {
-    wget https://katyayani-creations.in/$1 && mv $1 $1.html
-    changeURL $1
-}
+wget https://katyayani-creations.in/index.php && mv index.php index.html
 
-download "index" "index"
+for str in ${categoryArr[@]}; do
+    wget https://katyayani-creations.in/$str && mv $str $str.html
+done 
 
-downloadCategory "contact"
-downloadCategory "terms-and-conditions"
-downloadCategory "toys"
-downloadCategory "stationery"
-downloadCategory "gift-packs"
-downloadCategory "return-gifts"
+changeURL
 
 git add .
 git commit -am "update" && git push origin main
