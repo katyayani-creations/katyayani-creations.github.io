f4201ba0b0949849d3f0a17915983870d43d0848
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-19 20:59:40 +0530

    update

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-wget https://katyayani-creations.in/cat.php -o /dev/null  && mv cat.php cat.txt
-sed -i -e 's/##/\n/g' cat.txt
-
 function changeURL() {
     while IFS= read -r str; do
         sed -i -e 's/https:\/\/katyayani-creations.in\//https:\/\/katyayani-creations.github.io/g' $str.html
@@ -12,12 +8,27 @@ function changeURL() {
     sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' index.html
 }
 
-wget https://katyayani-creations.in/index.php -o /dev/null && mv index.php index.html
+# wget https://katyayani-creations.in/prod.php -o /dev/null  && mv prod.php prod.txt
+# sed -i -e 's/##/\n/g' prod.txt
+
+# wget https://katyayani-creations.in/cat.php -o /dev/null  && mv cat.php cat.txt
+# sed -i -e 's/##/\n/g' cat.txt
+
+# wget https://katyayani-creations.in/index.php -o /dev/null && mv index.php index.html
+
+# while IFS= read -r str; do
+#     echo "downloading file: $str"
+#     wget https://katyayani-creations.in/$str -o /dev/null && mv $str $str.html 
+# done < cat.txt
 
 while IFS= read -r str; do
+    arrIN=(${str//\// })
+    # echo ${arrIN[0]} 
+    eval "mkdir -p ${arrIN[0]}"
     echo "downloading file: $str"
-    wget https://katyayani-creations.in/$str -o /dev/null && mv $str $str.html 
-done < cat.txt
+    wget https://katyayani-creations.in/$str -o /dev/null -O $str  && mv $str $str.html 
+    break
+done < prod.txt
 
 changeURL
 
