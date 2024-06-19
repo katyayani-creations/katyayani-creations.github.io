88981c483febbbd64be2ef0ae6fff612f7916bb4
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-19 20:25:31 +0530

    update

--- bin/start.sh
+++ bin/start.sh
@@ -8,10 +8,10 @@ function changeURL() {
         while IFS= read -r str1; do
             sed -i -e 's/https:\/\/katyayani-creations.in\/'$str'/https:\/\/katyayani-creations.github.io\/'$str'/g' $str1.html
             sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' $str1.html
-        done < "$1"
+        done < cat.txt
         sed -i -e 's/https:\/\/katyayani-creations.in\/'$str'/https:\/\/katyayani-creations.github.io\/'$str'/g' index.html
         sed -i -e 's/G-6VBNE8F9XL/G-TMFL4Z8DEZ/g' index.html
-    done < "$1"    
+    done < cat.txt 
 }
 
 wget https://katyayani-creations.in/index.php && mv index.php index.html
@@ -19,10 +19,10 @@ wget https://katyayani-creations.in/index.php && mv index.php index.html
 while IFS= read -r str; do
     echo "Text read from file: $str"
     wget https://katyayani-creations.in/$str && mv $str $str.html
-done < "$1"
+done < cat.txt
 
 changeURL
 
 git add .
 git commit -am "update" && git push origin main
-rm -rf *.html-e
+rm -rf *-e
