bf3a0afae2ab4fc18ea4570198a11eb54c6830cb
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-19 18:20:02 +0530

    update

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,9 @@
 function download() {
     wget https://katyayani-creations.in/$1.php && mv $1.php $1.html
+    sed -i -e 's/https:\/\/katyayani-creations.in\/contact/https:\/\/katyayani-creations.github.io\/contact/g' $1.html
 }
 
 download "index"
 download "contact"
+
+git commit -am "update" && git push origin main
