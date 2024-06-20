ac761dff8f8e227b503545ae113280ccc6c4ac6b
Vishal Gupta <vishal.gupta@sourcefuse.com>
2024-06-20 20:34:36 +0530

    update

--- bin/commit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf *.html-e *.txt-e cat.txt prod.txt
-git add .
-git commit -am "update" && git push origin main
--- bin/downloadCat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ./bin/common.sh
-
-echo "downloading file: $1"
-wget https://katyayani-creations.in/$1 -o /dev/null && mv $1 $1.html 
-clean $1
